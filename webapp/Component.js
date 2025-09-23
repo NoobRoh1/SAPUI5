dd262ad740c6131ce4c7d3a84dc79c86da072a1d
Rohit Savadatti <rohit.savadatti@infosys.com>
2025-09-23 18:28:34 +0000

    Completeflow

---
+++ webapp/Component.js
@@ -0,0 +1,27 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "com/demo/sapui5/model/models"
+], 
+(UIComponent, models) => {
+    "use strict";
+
+    return UIComponent.extend("com.demo.sapui5.Component", {
+        metadata: {
+            manifest: "json",
+            interfaces: [
+                "sap.ui.core.IAsyncContentCreation"
+            ]
+        },
+
+        init() {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
+
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
+
+            // enable routing
+            this.getRouter().initialize();
+        }
+    });
+});
