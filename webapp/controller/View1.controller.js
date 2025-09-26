3bb0aa806b35f7aada3f932909b5f02f3a0cf3d5
Rohit Savadatti <rohit.savadatti@infosys.com>
2025-09-26 20:09:07 +0000

    Dynamic UI elements

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,6 +8,17 @@ sap.ui.define([
         },
       Press() {
         this.getOwnerComponent().getRouter().navTo("RouteView2")
-      }
+      },
+      Saved() {
+        var name = this.getView().byId('Inputf').getValue();
+        var name1 = this.getView().byId('Inputs').getValue();
+        var newname = "Hello " + name +  name1 + "! your details are saved successfully!!";
+
+        this.getView().byId('main').setText(newname);
+        this.getView().byId('save');
+        this.getView().byId('save').setType("Reject");
+      },
+      
+         
     });
 });
